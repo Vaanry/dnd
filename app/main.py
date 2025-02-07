9e9b3f798d048203df8a46c2958031e405f84c25
Runeolf <runeolf1488@gmail.com>
2025-02-07 18:16:40 +0300

    add_style_index

--- app/main.py
+++ app/main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 
 from app.routers import main_router
 
 #from .config import settings
-
+templates = Jinja2Templates(directory="templates")
 app = FastAPI(title='DND')
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def welcome(request: Request) -> dict:
-    return {"Hello": "FastAPI"}
-
+    return templates.TemplateResponse(
+        "index.html", {"request": request}
+    )
 
 app.include_router(main_router)
